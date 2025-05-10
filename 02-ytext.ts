0426a5a12fbdb6f2699ea4c1d66e69324a2045a2
Stephan Kulla <git.mail@kulla.me>
2025-05-10 18:04:03 +0200

    feat: Text yText.insertEmbed()

--- 02-ytext.ts
+++ 02-ytext.ts
@@ -9,4 +9,10 @@ ytext.insert(5, " World")
 ytext.format(0, 5, { bold: true })
 ytext.delete(0, 2)
 
+ytext.insertEmbed(ytext.toString().length, {
+  type: "image",
+  src: "https://example.com/image.png",
+})
+
+console.log(ytext.toString())
 console.log(ytext.toDelta())
